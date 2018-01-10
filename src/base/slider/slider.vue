0aac1e34d7c144fb42a6fb455c56de91c0321c10
zhangYuFan <zhangyufanzyf@qq.com>
2018-01-10 17:03:51 +0800

    4-6轮播组件结束
    增加 DOT 圆点轮播下标 通过currentPageIndex确定当前下标
    自动轮播 定时器
    使用Window对象当页面大小改变时轮播也改变大小

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -7,7 +7,7 @@
       </slot>
     </div>
     <div class="dots">
-      <span class="dot"></span>
+      <span class="dot" v-for="(item, index) in dots" :class="{active: currentPageIndex === index}"></span>
     </div>
   </div>
 </template>
@@ -17,6 +17,12 @@
   import {addClass} from 'common/js/dom' // 为swiper添加class
 
   export default {
+    data() {
+      return {
+        dots: [], // 轮播下标
+        currentPageIndex: 0 // 当前第几页
+      }
+    },
     props: {
       loop: { // 是否循环
         type: Boolean,
@@ -28,17 +34,30 @@
       },
       interval: { // 轮播时间间隔
         type: Number,
-        default: 4000
+        default: 3000
       }
     },
     mounted() { // 编译好的HTML挂载到页面 的 mounted钩子
       setTimeout(() => {
         this._setSliderWidth()
+        this._initDots() // 轮播下标 因为 children.length 需要在初始化前完成
         this._initSlider()
+
+        if (this.autoPlay) { // 如果开启自动循环
+          this._play()
+        }
       }, 20)
+
+      window.addEventListener('resize', () => { // 监听窗口大小改变时的事件
+        if (!this.slider) {
+          return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh() // BScroll 重新计算轮播组件宽度大小
+      })
     },
     methods: {
-      _setSliderWidth() { // 设置slider宽度
+      _setSliderWidth(isResize) { // 设置slider宽度   isResize => 是否是第一次加载宽度
         this.children = this.$refs.sliderGroup.children // 获取轮播图的数量 DOM对象的children？
 
         let width = 0 // 整个轮播的总长度
@@ -51,22 +70,46 @@
           width += sliderWidth
         }
 
-        if (this.loop) {
+        if (this.loop && !isResize) { // isResize第二次页面加载此方法不能双倍width
           width += 2 * sliderWidth
         }
         this.$refs.sliderGroup.style.width = width + 'px'
       },
+      _initDots() { // 初始化轮播下标
+        this.dots = new Array(this.children.length) // 长度为5的空数组
+      },
       _initSlider() { // 初始化slider
-        this.slider = new BScroll(this.$refs.slider, { // BScroll组件各种参数
+        this.slider = new BScroll(this.$refs.slider, { // BScroll组件各种参数 将BScroll 实例化进slider变量里
           scrollX: true,
           scrollY: false,
           momentum: false,
           snap: true,
           snapLoop: this.loop,
           snapThreshold: 0.3,
-          snapSpeed: 400,
-          click: true
+          snapSpeed: 400
         })
+
+        this.slider.on('scrollEnd', () => { // BScroll可以绑定一个事件 确定滚动到第几个元素
+          let pageIndex = this.slider.getCurrentPage().pageX
+          if (this.loop) { // 如果循环 BScroll 组件会自动在前后各多生成一个新元素 则需要 -1
+            pageIndex -= 1
+          }
+          this.currentPageIndex = pageIndex
+
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+            this._play()
+          }
+        })
+      },
+      _play() { // 自动轮播
+        let pageIndex = this.currentPageIndex + 1
+        if (this.loop) {
+          pageIndex += 1
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400) // BScroll 的 goToPage 方法 跳转至某一页
+        }, this.interval)
       }
     }
   }
