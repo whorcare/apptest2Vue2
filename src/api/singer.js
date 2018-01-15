32347f9a60dca991d6987b06c0c8894130579462
zhangYuFan <zhangyufanzyf@qq.com>
2018-01-15 15:06:44 +0800

    5-2 获取歌手列表数据

---
+++ src/api/singer.js
@@ -0,0 +1,21 @@
+// 获取歌手页面数据
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getSingerList() { // 获取歌手列表
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    g_tk: 1664029744
+  })
+
+  return jsonp(url, data, options)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,9 +5,35 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
 
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() { // 获取歌手列表数据
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this.singers)
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
