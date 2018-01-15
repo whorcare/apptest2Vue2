88089bc862dc40183f3dae1dbd975af186ec7fe0
zhangYuFan <zhangyufanzyf@qq.com>
2018-01-15 17:21:58 +0800

    5-4 listview 歌手列表组件 基础组件开发 实现滚动
    1 双重循环渲染
    2 scroll 组件使用
    3 懒加载v-lazy

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer">
-    歌手页面
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
@@ -8,6 +8,7 @@
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
 
   const HOT_NAME = '热门'
   const HOT_SINGER_LEN = 10
@@ -25,8 +26,7 @@
       _getSingerList() { // 获取歌手列表数据
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-            this.singers = res.data.list
-            console.log(this._normalizeSinger(this.singers))
+            this.singers = this._normalizeSinger(res.data.list)
           }
         })
       },
@@ -75,6 +75,9 @@
         // 将热门与普通数据拼接在一起
         return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
 </script>
