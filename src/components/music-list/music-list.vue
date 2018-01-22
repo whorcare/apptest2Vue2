10b52b9859a5b8a42fd8ea6fb4170dc6d9a74828
zhangYuFan <zhangyufanzyf@qq.com>
2018-01-22 14:42:44 +0800

    6章结束
    1 返回上一个页面按钮
    2 loading组件
    3 随机播放全部btn

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,11 +1,18 @@
 <!-- 多个使用的 歌曲详情组件 -->
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title">{{title}}</h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <!--随机播放按钮-->
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <!--todo 向上推动时需要的层 layer -->
@@ -14,6 +21,9 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -21,6 +31,7 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+  import Loading from 'base/loading/loading'
   import {prefixStyle} from 'common/js/dom'
 
   const RESERVED_HEIGHT = 40 // 顶部title高度
@@ -65,6 +76,9 @@
     methods: {
       scroll(pos) { // 接收scroll的监听事件
         this.scrollY = pos.y
+      },
+      back() { // 返回上一页
+        this.$router.back()
       }
     },
     watch: {
@@ -86,9 +100,11 @@
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
         this.$refs.bgImage.style[transform] = `scale(${scale})`
@@ -96,7 +112,8 @@
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     }
   }
 </script>
