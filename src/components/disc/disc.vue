981c18bfb4f8621223cb7feee35c3c9d14467b60
zhangYuFan <zhangyufanzyf@qq.com>
2018-01-31 17:11:25 +0800

    8-3歌单详情页数据的处理和应用
    1 处理数据
    2 数据加载传入

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,7 +1,7 @@
 <!--首页歌单详情页-->
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -10,8 +10,14 @@
   import {mapGetters} from 'vuex'
   import {getSongList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
 
   export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
     computed: {
       title() {
         return this.disc.dissname
@@ -28,11 +34,24 @@
     },
     methods: {
       _getSongList() {
+        if (!this.disc.dissid) {
+          this.$router.push('recommend')
+          return
+        }
         getSongList(this.disc.dissid).then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res)
+            this.songs = this._normalizeSongs(res.cdlist[0].songlist)
           }
         })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albumid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
       }
     },
     components: {
