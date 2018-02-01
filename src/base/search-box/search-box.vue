a955e4eb52ea18e260d0e5cbf9759d59e409e4c0
zhangYuFan <zhangyufanzyf@qq.com>
2018-02-01 17:29:42 +0800

    10-2搜索页面search-box组件开发
    1 添加search-box搜索栏组件
    2 placeholder props 传递
    3 双向绑定v-model  query
    4 当query变化时watch状态 并向外部派发$emit事件

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,63 @@
+<!--搜索框组件-->
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="box" v-model="query" :placeholder="placeholder"/>
+    <i @click="clear" v-show="query" class="icon-dismiss"></i><!--有query时才会显示 X -->
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      placeholder: {
+        type: String,
+        default: '搜索歌曲，歌手'
+      }
+    },
+    data() {
+      return {
+        query: '' // 双向绑定query
+      }
+    },
+    methods: {
+      clear() {
+        this.query = ''
+      }
+    },
+    created() {
+      this.$watch('query', (newQuery) => { // 监听query改变事件 写在created钩子中 向外部组件派发$emit事件
+        this.$emit('query', newQuery)
+      })
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .search-box
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    width: 100%
+    padding: 0 6px
+    height: 40px
+    background: $color-highlight-background
+    border-radius: 6px
+    .icon-search
+      font-size: 24px
+      color: $color-background
+    .box
+      flex: 1
+      margin: 0 5px
+      line-height: 18px
+      background: $color-highlight-background
+      color: $color-text
+      font-size: $font-size-medium
+      &::placeholder
+        color: $color-text-d
+    .icon-dismiss
+      font-size: 16px
+      color: $color-background
+</style>
